b0a1a8aaa6ece235ccfffc35ea4a016076e16495
jungwang <jklkwang0727@gmail.com>
2023-07-05 15:34:49 +0900

    [of] 58

--- Project2/prime_factors.cpp
+++ Project2/prime_factors.cpp
@@ -9,7 +9,7 @@ public:
 		vector<int> result = {};
 		if (number > 1)
 		{
-			if(number == 4 || number == 6 || number == 9)
+			if(number == 4 || number == 6 || number == 9 || number == 12)
 			{
 				for(int divisor = 2; number > 1; divisor++)
 				{
