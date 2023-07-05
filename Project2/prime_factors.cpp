fec33834973c92a1fb602616b10bcb2e182d1482
jungwang <jklkwang0727@gmail.com>
2023-07-05 15:35:44 +0900

    [refactoring] 59

--- Project2/prime_factors.cpp
+++ Project2/prime_factors.cpp
@@ -9,18 +9,13 @@ public:
 		vector<int> result = {};
 		if (number > 1)
 		{
-			if(number == 4 || number == 6 || number == 9 || number == 12)
+			for (int divisor = 2; number > 1; divisor++)
 			{
-				for(int divisor = 2; number > 1; divisor++)
-				{
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
 			}
-			else
-				result.push_back(number);
 		}
 		
 		return result;
