4c13ff6abd3f0e5a2f7f13f685acd6779325e847
jungwang <jklkwang0727@gmail.com>
2023-07-05 15:24:05 +0900

    [refactoring] 45 ~ 46

--- Project2/prime_factors.cpp
+++ Project2/prime_factors.cpp
@@ -9,10 +9,12 @@ public:
 		vector<int> result = {};
 		if (number > 1)
 		{
-			if(number == 4)
-			{
-				result.push_back(2);
-				result.push_back(2);
+			if (number == 4) {
+				while(number % 2 == 0)
+				{
+					result.push_back(2);
+					number /= 2;
+				}
 			}
 			else
 				result.push_back(number);
