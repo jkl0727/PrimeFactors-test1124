616ab440d0c665c2d74ff32f7679e25bcdcedf68
jungwang <jklkwang0727@gmail.com>
2023-07-05 15:31:34 +0900

    [of] 54

--- Project2/prime_factors.cpp
+++ Project2/prime_factors.cpp
@@ -25,6 +25,11 @@ public:
 					number /= divisor;
 				}
 			}
+			else if (number == 9)
+			{
+				result.push_back(3);
+				result.push_back(3);
+			}
 			else
 				result.push_back(number);
 		}
